50092b2d622fbf1e6706da957c39ee08deb38bc9
WilDevp <wilmar.garciava@gmail.com>
2023-04-23 16:11:45 -0500

    feat: :art: Initial commit
    Se configura la estrucutra de paquetes y conexion a MYSQL
    
    WORKING: 01

---
+++ src/main/java/com/wildevp/app/ArquitecturaHexagonalSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.wildevp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquitecturaHexagonalSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArquitecturaHexagonalSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wildevp/app/ArquitecturaHexagonalSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildevp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquitecturaHexagonalSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
